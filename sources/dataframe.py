a29e85dac0847d64115472865216df8903c691ed
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-25 23:18:14 -0600

    getting histograms for each numerial attribute

--- sources/dataframe.py
+++ sources/dataframe.py
@@ -1,6 +1,9 @@
 from dataset_retrieve import load_housing_data
+from matplotlib import pyplot as plt 
 
 housing = load_housing_data()
 print(housing.head(5))
 print(housing.shape)
 print(housing.describe())
+housing.hist(bins=50, figsize=(20,15))
+plt.show()
